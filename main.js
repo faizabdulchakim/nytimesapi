45269fbb284d467ec4bc5d895201c85ed497874e
faizabdulchakim <faizabdulchakim@gmail.com>
2020-12-19 15:40:47 +0700

    nyt pasring

--- main.js
+++ main.js
@@ -7,9 +7,16 @@ var app				= express();
 //app.use(step1);
 
 app.get('/', function(req, res){
-	axios.get('http://localhost:80/api/step1.php')
+	axios.get('https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20201201&end_date=20201201&q=election&api-key=h2PhtyLa0ScGTMGtH8K9GGQTJlGR0wup')
 	.then(function (response) {
-		console.log(response.data)
+		//title
+		console.log(response.data.response.docs[0].headline.main)
+		console.log(response.data.response.docs[0].abstract);
+		console.log("https://static01.nyt.com/"+response.data.response.docs[0].multimedia[0].url);
+		console.log(response.data.response.docs[0].lead_paragraph);
+		
+		
+		
 	})
 	.catch(function (error) {
 		console.log(error)
